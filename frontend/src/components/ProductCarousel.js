36282a4492ba336ecbf0415b8be4e220069c4be6
Prakhar Sankrityayan <prakhar3099@gmail.com>
2022-09-26 21:22:26 +0530

    fix: Carousel fixed

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -22,18 +22,19 @@ const ProductCarousel = () => {
     <Message variant="danger">{error}</Message>
   ) : (
     <Carousel pause="hover" className="bg-dark">
-      {products.map((product) => (
-        <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption className="carousel-caption">
-              <h2>
-                {product.name} ({product.price})
-              </h2>
-            </Carousel.Caption>
-          </Link>
-        </Carousel.Item>
-      ))}
+      {products &&
+        products.map((product) => (
+          <Carousel.Item key={product._id}>
+            <Link to={`/product/${product._id}`}>
+              <Image src={product.image} alt={product.name} fluid />
+              <Carousel.Caption className="carousel-caption">
+                <h2>
+                  {product.name} ({product.price})
+                </h2>
+              </Carousel.Caption>
+            </Link>
+          </Carousel.Item>
+        ))}
     </Carousel>
   );
 };
